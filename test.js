7cf57c684c18baf8ffeb4d070d800f5c2df7f51a
lolme <kai.eba_to11@outlook.jp>
2023-04-24 23:07:05 +0900

    04-24

--- test.js
+++ test.js
@@ -1,7 +1,44 @@
-var maxInt = 100;
-var minInt = -100;
-var randomInt = Math.floor ( Math.random() * (maxInt + 1 - minInt) ) + minInt ;
-if(randomInt < 0){
-  randomInt = "(" + randomInt + ")";
-};
-console.log(randomInt);
+console.log(-68%97);
+function answerQuestion(clause1, operater, clause2){
+  switch(operater){
+    case "+":
+      return clause1 + clause2;
+    case "-":
+      return clause1 - clause2;
+    case "×":
+      return clause1 * clause2;
+    case "÷":
+      var division =  Math.floor(clause1 / clause2);
+      var remainder = clause1 % clause2;
+      return division + " 余り " + remainder;
+    default:
+      break;
+  }
+}
+
+function createOperater(){
+  var operater = ["+" , "-" , "×" , "÷"] ;
+  var randomInt = Math.floor ( Math.random() * (3 + 1 - 0) ) + 0 ;
+  return operater[randomInt] ;
+}
+function answerQuestion(clause1, operater, clause2){
+  switch(operater){
+    case "+":
+      return clause1 + clause2;
+    case "-":
+      return clause1 - clause2;
+    case "×":
+      return clause1 * clause2;
+    case "÷":
+      if (clause1 < 0){
+        clause1 = clause1 * -1;
+      }else if(clause2 < 0){
+        clause2 = clause2 * -1;
+      }
+      var division =  clause1 / clause2 | 0;
+      var remainder = clause1 % clause2;
+      return division + " 余り " + remainder;
+    default:
+      break;
+  }
+}
